99fe7267fecfebe8b7cc25cf744c9b1e457adb95
Mose <km_413@naver.com>
2023-02-17 14:39:21 +0900

    Adjusting Chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/widgets/Chart_bar.dart';
 import '/models/transaction.dart';
 import 'package:intl/intl.dart';
 
@@ -21,9 +22,6 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E().format(weekDay));
-      print(totalSum);
-
       return {
         "DAY": DateFormat.E().format(weekDay),
         "AMOUNT": totalSum
@@ -31,16 +29,32 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupTransactionValues.fold(0.0, (sum, data) {
+      return sum + data['AMOUNT'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    print(groupTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children:   <Widget> [
-          
-        ],
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                data['DAY'], 
+                data['AMOUNT'], 
+                totalSpending == 0.0 ? 0.0 : (data['AMOUNT'] as double)/totalSpending
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
